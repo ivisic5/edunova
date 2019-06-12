7a54aed85205b28a0fc2e084f39233b7ff27e72d
ivisic5 <visic.ivan1@gmail.com>
2019-06-12 15:56:27 +0200

    ispravak

--- hotel/hotel.sql
+++ hotel/hotel.sql
@@ -2,17 +2,19 @@ drop database if exists Hotel;
 create database Hotel default character set utf8;
 use  Hotel;
 
-create table opis_sobe(
+
+create table soba_id(
 id int not null auto_increment primary key,
-opis decimal(18,2)
+kategorija varchar(30) not null,
+kvadratura int not null,
+opis_sobe varchar(200),
+cijena decimal(18,2) not null
 );
 
 create table soba(
 id int not null auto_increment primary key,
-kategorija varchar(30) not null,
-kvadratura int not null,
-opis_sobe int,
-cijena decimal(18,2) not null
+rezervacija int,
+soba_id int
 );
 
 create table vrsta_posla(
@@ -29,9 +31,10 @@ vrsta_posla int not null
 );
 
  
-create table gosti(
+create table gost(
 oib char(11) not null primary key,
 ime varchar(50) not null,
+broj_telefona char(20),
 prezime varchar(50) not null
 );
 
@@ -41,23 +44,24 @@ datum date not null,
 cijena decimal(18,2) not null
 );
 
-create table loglist(
+create table rezervacija(
 id int not null auto_increment primary key,
 soba int not null,
-gosti char not null,
-osoblje int not null,
+gost char not null,
+broj_gostiju int not null,
 pocetak date not null,
 kraj date not null,
 racun int not null
 );
 
-
-alter table soba add foreign key (opis_sobe) references opis_sobe(id);
+alter table soba add foreign key (soba_id) references soba_id(id);
+alter table soba add foreign key (rezervacija) references rezervacija(id);
 alter table osoblje add foreign key (vrsta_posla) references vrsta_posla(id);
-alter table loglist add foreign key (soba) references soba(id) ;
-alter table loglist add foreign key (osoblje) references osoblje(id) ;
-alter table loglist add foreign key (gosti) references gosti(oib);
-alter table loglist add foreign key (racun) references racun(id);
+alter table rezervacija add foreign key (gost) references gost(oib);
+alter table rezervacija add foreign key (racun) references racun(id);
+
+
+
 
 
 
